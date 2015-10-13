0ac3e52a1949113b163ee0769c61e8c9a6e97816
Ben Patterson <bpatterson@edx.org>
2015-10-13 17:33:04 -0400

    Better hinting on test_build_server.

--- playbooks/roles/test_build_server/files/test-development-environment.sh
+++ playbooks/roles/test_build_server/files/test-development-environment.sh
@@ -14,34 +14,44 @@ set -e
 
 # Doing this rather than copying the file into the scripts folder so that
 # this file doesn't get cleaned out by the 'git clean' in all-tests.sh.
+echo "----cloning edx-platform"
 cd edx-platform-clone
 
 # This will run all of the setup it usually runs, but none of the
 # tests because TEST_SUITE isn't defined.
+echo "----sourcing jenkins-common"
 source scripts/jenkins-common.sh
 
 # Now we can run a subset of the tests via paver.
 # Run some of the common/lib unit tests
+echo "----paver test_lib -t common/lib/xmodule/xmodule/tests/test_stringify.py"
 paver test_lib -t common/lib/xmodule/xmodule/tests/test_stringify.py
 
 # Generate some coverage reports
+echo "----paver coverage"
 paver coverage
 
 # Run some of the djangoapp unit tests
+echo "----paver test_system for lms"
 paver test_system -t lms/djangoapps/courseware/tests/tests.py
+echo "----paver test_system for cms"
 paver test_system -t cms/djangoapps/course_creators/tests/test_views.py
 
 # Run some of the javascript unit tests
+echo "----paver test_js_run for xmodule"
 paver test_js_run -s xmodule
 
 # Run some of the bok-choy tests
+echo "----three bokchoy tests"
 paver test_bokchoy -t discussion/test_discussion.py:DiscussionTabSingleThreadTest
 paver test_bokchoy -t studio/test_studio_with_ora_component.py:ORAComponentTest --fasttest
 paver test_bokchoy -t lms/test_lms_matlab_problem.py:MatlabProblemTest --fasttest
 
 # Run some of the lettuce acceptance tests
+echo "----test_acceptance (lettuce)"
 paver test_acceptance -s lms --extra_args="lms/djangoapps/courseware/features/problems.feature -s 1"
 paver test_acceptance -s cms --extra_args="cms/djangoapps/contentstore/features/html-editor.feature -s 1"
 
 # Generate quality reports
+echo "----paver run_quality "
 paver run_quality
