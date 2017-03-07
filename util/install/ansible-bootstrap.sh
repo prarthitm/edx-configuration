b3d8ebefe971f6ccddce00ff9a9eaf4d5eb568b3
Manikarthik Konakalla (Infosys Ltd) <v-mankon@microsoft.com>
2017-03-07 12:19:54 -0800

    chnaging branch names

--- util/install/ansible-bootstrap.sh
+++ util/install/ansible-bootstrap.sh
@@ -22,11 +22,11 @@ if [[ -z "${ANSIBLE_VERSION}" ]]; then
 fi
 
 if [[ -z "${CONFIGURATION_REPO}" ]]; then
-  CONFIGURATION_REPO="https://github.com/edx/configuration.git"
+  CONFIGURATION_REPO="https://github.com/microsoft/edx-configuration.git"
 fi
 
 if [[ -z "${CONFIGURATION_VERSION}" ]]; then
-  CONFIGURATION_VERSION="master"
+  CONFIGURATION_VERSION="multiple_orgs_mani"
 fi
 
 if [[ -z "${UPGRADE_OS}" ]]; then
