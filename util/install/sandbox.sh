47cede44a7d1b5c486cf0ea98fa9ec28f0697986
Manikarthik Konakalla (Infosys Ltd) <v-mankon@microsoft.com>
2017-03-07 12:32:23 -0800

    change the repo url and versions

--- util/install/sandbox.sh
+++ util/install/sandbox.sh
@@ -72,7 +72,7 @@ CONFIGURATION_VERSION=${CONFIGURATION_VERSION-${OPENEDX_RELEASE-master}}
 ## Clone the configuration repository and run Ansible
 ##
 cd /var/tmp
-git clone https://github.com/edx/configuration
+git clone https://github.com/microsoft/edx-configuration configuration
 cd configuration
 git checkout $CONFIGURATION_VERSION
 git pull
